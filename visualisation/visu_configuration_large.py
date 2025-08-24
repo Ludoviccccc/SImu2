5a2df0994e8814bc8dfba12469373a7a462cd0ae
Ludoviccccc <ludovic.matar@yahoo.com>
2025-08-24 13:30:13 +0200

    actu images + computation imgep k2 3 5

--- visualisation/visu_configuration_large.py
+++ visualisation/visu_configuration_large.py
@@ -7,15 +7,15 @@ if __name__ == "__main__":
     image_folder ="all_images/image_large"
     N = int(10000)
     N_init = 1000
-    ks = [1,2,3,5,20]
+    ks = [2,3,5]
     num_bank = 4
     num_addr = 20
     file_mix = lambda k,N: f"history_weak_{k}_N_{N}_0.pkl"
-    file_imgep_ir = lambda k,N: f"history_kNN_{k}_N_{N}_lp_0.pkl"
+    file_imgep_ir = lambda k,N: f"history_kNN_{k}_N_{N}_lp_1.pkl"
     file_imgep_no_ir = lambda k,N: f"history_kNN_{k}_N_{N}_no_lp_0.pkl"
     files = []
 #    files +=[{"folder":folder[2],"file":file_mix(k,N),"name":f"mix_programs k={k},N={N}","k":k,"N":N,"type":"mix_programs"} for k in ks]
-#    files +=[{"folder":folder[0],"file":file_imgep_ir(k,N),"name":f"imgep_ir k={k},N={N}","k":k,"N":N,"type":"imgep_ir"} for k in ks]
+    files +=[{"folder":folder[0],"file":file_imgep_ir(k,N),"name":f"imgep_ir k={k},N={N}","k":k,"N":N,"type":"imgep_ir"} for k in ks]
     files +=[{"folder":folder[1],"file":file_imgep_no_ir(k,N),"name":f"imgep_no_ir k={k},N={N}","k":k,"N":N,"type":"imgep_no_ir"} for k in ks]
     if os.path.exists(os.path.join("../",image_folder))==False:
         os.system(f"mkdir {image_folder}")
